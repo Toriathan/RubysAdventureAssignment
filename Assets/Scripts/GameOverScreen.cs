3a57fef25d24035740c4ae9f2895ce39ef36c787
Toriathan <fyivgukugvhkj@gmail.com>
2023-12-06 00:04:37 -0500

    OneLastCommit

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,30 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public static GameOverScreen instance { get; private set; }
+    
+    
+    
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    public void CheckValue(float value)
+    {				      
+        if (value < 1)
+        {
+
+        }
+    }
+}
